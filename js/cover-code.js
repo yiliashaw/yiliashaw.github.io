f1d11431c21055c3054eadadc9e743468c5a667f
yiliashaw <yilia.shaw@gmail.com>
2017-03-18 20:58:40 +0800

    modify type speed

--- js/cover-code.js
+++ js/cover-code.js
@@ -1,7 +1,7 @@
 const codeType = function() {
     Typed.new(".code", {
         strings: ["<i>Try Not to Become a Man of Success.</i></br><i> Rather Become a Man of Value. </i> </br> <i class='author'>—— Albert Einstein</i>"],
-        typeSpeed: 100,
+        typeSpeed: 60,
     });
 };
 
